666fbec1ea4dbbbd3a84c3327e9a56eb67f504ad
yarden <nagel.yarden@gmail.com>
2020-01-05 10:16:21 +0200

    fixed edit route

--- routes/comments.js
+++ routes/comments.js
@@ -50,7 +50,7 @@ router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, 
         if (err){
             res.redirect("back")
         } else {
-            res.render("/comments/edit",{campground_id: req.params.id, comment: foundComment});
+            res.render("comments/edit",{campground_id: req.params.id, comment: foundComment});
         }
     });
 })
